68bc4115011cd255ac756ff1f2473dc61e86c664
huicpc0215 <huicpc0215@gmail.com>
2014-10-31 23:50:56 +0800

    add basic BloomFilter

---
+++ BloomFilter.h
@@ -0,0 +1,15 @@
+/*=============================================================================
+#     FileName: BloomFilter.h
+#         Desc: Basic units in the BloomFilter
+#       Author: WenShi(huicpc0215)
+#        Email: huicpc0215@gmail.com
+#     HomePage: https://github.com/huicpc0215
+#      Version: 0.0.1
+#   LastChange: 2014-10-31 23:49:48
+#      History:
+=============================================================================*/
+
+class BloomFilterCell{
+
+
+};
