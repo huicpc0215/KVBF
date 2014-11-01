67035db9784f78d8e10a155c6d184e61d85eda5e
huicpc0215 <huicpc0215@gmail.com>
2014-11-01 22:37:37 +0800

    update Bloom Filter Cell and Bloom Filter

---
+++ utility/BloomFilter.h
@@ -0,0 +1,14 @@
+/*=============================================================================
+#     FileName: BloomFilter.h
+#         Desc: description for Bloom Filter
+#       Author: WenShi(huicpc0215)
+#        Email: huicpc0215@gmail.com
+#     HomePage: https://github.com/huicpc0215
+#      Version: 0.0.1
+#   LastChange: 2014-11-01 22:30:43
+#      History:
+=============================================================================*/
+#define CellNumber 1000
+class BloomFilter{
+    BloomFilterCell B[CellNumber];
+};
--- utility/BloomFilterCell.h
+++ utility/BloomFilterCell.h
@@ -10,6 +10,17 @@
 =============================================================================*/
 
 class BloomFilterCell{
-
-
+    //use cnt to count the number of how many digits has hashed into this
+    //type : int
+    //0: the element has just insert into the Cell
+    //10: initiation value , means that the information has no use
+    int cnt;
+    //use pointer to  another single Cell that save one Cell
+    //type:  int
+    //-1: initiation  with no use in the cell
+    //0-m-1: the next single Cell
+    //m: the Cell has two or more information.
+    int next;
 };
+
+
